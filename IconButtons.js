5c2c9de6940428527d13c2b08695c2bd7269d9d5
Jun Woong <woong.jun@gmail.com>
2016-05-15 15:58:19 +0900

    Fixed a bug to correctly access to event handler

--- IconButtons.js
+++ IconButtons.js
@@ -46,7 +46,7 @@ module.exports = React.createClass({
 
         return (
             <div className={'buttons--root '+(this.props.className || '')}
-                 onTouchTap={this.handler}>
+                 onTouchTap={this.props.handler}>
                 {Buttons}
             </div>
         )
